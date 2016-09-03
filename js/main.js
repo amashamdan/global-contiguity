a09b440f7f453506b4d5652664707cac0b13d1dc
Amer Hamdan <amashamdan@gmail.com>
2016-09-02 17:43:55 -0700

    Feat: Added nodes and link.
    The chart now has a node representing every country and all the links corresponding to a shared border.
    Nodes all red at this point, flags will be added at a later point.
    Next, a tooltip will be shown when a node is hovered showing the name of the country.

--- js/main.js
+++ js/main.js
@@ -9,12 +9,44 @@ $.ajax({
 	}
 });
 
-function plot(data) {
+function plot(dataSet) {
 	var chartWidth = 900;
-	chartHeight = 700;
-	var chart = d3.select("#chart-area")
+	var chartHeight = 700;
+	var svg = d3.select("#chart-area")
 				.append("svg")
 				.attr("width", chartWidth)
 				.attr("height", chartHeight)
-				.style("background-color", "rgb(255, 240, 240)");
+				.style("background-color", "rgba(0, 0, 0, 0.2)");
+
+	var force = d3.layout.force()
+					.nodes(dataSet.nodes)
+					.links(dataSet.links)
+					.size([chartWidth, chartHeight])
+					.linkDistance([25])
+					.charge([-75])
+					.start();
+
+	var edges = svg.selectAll("line")
+					.data(dataSet.links)
+					.enter()
+					.append("line")
+					.style("stroke", "green")
+					.style("stroke-width", 1);
+
+	var nodes = svg.selectAll("circle")
+					.data(dataSet.nodes)
+					.enter()
+					.append("circle")
+					.attr("r", 5)
+					.style("fill", "red")
+					.call(force.drag);
+
+	force.on("tick", function() {
+		edges.attr("x1", function(d) {return d.source.x;})
+			.attr("y1", function(d) {return d.source.y;})
+			.attr("x2", function(d) {return d.target.x;})
+			.attr("y2", function(d) {return d.target.y;});
+		nodes.attr("cx", function(d) {return d.x;})
+			.attr("cy", function(d) {return d.y;});
+	})
 }
